79939ac6c8e5e06ae8b7b3f41216847895509142
afrojaakter032 <afroja.cu.cse@gmail.com>
2024-05-18 10:16:43 +0600

    project create and tailwind configure

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,20 @@
+function Home() {
+    return (
+        <>
+            <section className="hero-section">
+                <div className="container bg-slate-500 mx-auto">
+                    <div className="grid grid-cols-2">
+                        <div>
+                        content-lef
+                        </div>
+                        <div>
+                        content-right
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Home;
